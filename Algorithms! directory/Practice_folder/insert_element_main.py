ab5c817f25ee8a3308239369abedb908567260a5
Shafqat-Shahriar <60550696+Shafqat-Shahriar@users.noreply.github.com>
2024-11-13 15:44:59 +0600

    Create insert_element_main.py

---
+++ Algorithms! directory/Practice_folder/insert_element_main.py
@@ -0,0 +1,21 @@
+# Python program for inserting
+# an element in unsorted array
+
+# method to insert element
+
+
+
+
+
+# Driver's code
+if __name__ == '__main__':
+   # declaring array and key to insert
+   arr  = [12, 16, 20, 40, 50, 70]
+   key = 26
+
+   # array before inserting an element
+   print("Before Inserting: ")
+   print(arr)
+
+   insertEnd(arr, key)
+   print(arr)
